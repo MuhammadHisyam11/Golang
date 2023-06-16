e1678a782fff3715586554e600ca5b7281a594ec
Muhammad Hisyam <91212724+MuhammadHisyam11@users.noreply.github.com>
2023-06-16 15:32:16 +0700

    Add files via upload

---
+++ Go/constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		first  string = "hello"
+		second        = "world"
+		value         = 1000
+	)
+
+	fmt.Println(first)
+	fmt.Println(second)
+	fmt.Println(value)
+}
---
+++ Go/variable.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Muhammad Hisyam"
+	fmt.Println(name)
+
+	var friendname = "Agus"
+	fmt.Println(friendname)
+
+	country := "Indonesia"
+	fmt.Println(country)
+}
